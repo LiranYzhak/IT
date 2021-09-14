2c67324f0036a9eed43374d37d6dd6e7f9ab317d
LiranYzhak <34236353+LiranYzhak@users.noreply.github.com>
2021-09-14 14:04:51 +0300

    Update blendwallaper.ps1

--- blendwallaper.ps1
+++ blendwallaper.ps1
@@ -1,6 +1,6 @@
 cd C:\Windows\Web\Screen
 $CheckFile = Test-Path C:\Windows\Web\Screen\blendwallpaper.reg
-if ($CheckFile) {exit} else{continue}
+if ($CheckFile) {exit} else {continue}
 wget https://github.com/LiranYzhak/IT/raw/main/blendwallpaer.png -outfile blendwallpaer.png
 wget https://raw.githubusercontent.com/LiranYzhak/IT/main/blendwallpaper.reg -OutFile blendwallpaper.reg
 reg import ./blendwallpaper.reg
