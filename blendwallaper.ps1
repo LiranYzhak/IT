d40be1ed9d7923b067330f08daf00f4ec92eb5ab
LiranYzhak <34236353+LiranYzhak@users.noreply.github.com>
2021-09-14 11:28:28 +0300

    Update blendwallaper.ps1

--- blendwallaper.ps1
+++ blendwallaper.ps1
@@ -1,2 +1,2 @@
 ﻿cd C:\Windows\Web\Screen
-wget https://i.ibb.co/J5xV3f8/wallpaper.png -outfile blendwallpaer.png
+wget https://github.com/LiranYzhak/IT/raw/main/blendwallpaer.png -outfile blendwallpaer.png
